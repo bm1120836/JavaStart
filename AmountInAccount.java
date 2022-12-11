e3d61a5f5aa2893d8152bb5783965dde8b6d7f3d
BM <bm1120836@yandex.ru>
2022-12-11 11:14:27 +0300

    Программа, в которой применяется переменная

---
+++ AmountInAccount.java
@@ -0,0 +1,18 @@
+/*
+ * Программа, в которой применяется переменная
+ */
+
+ public class AmountInAccount {
+ 
+    public static void main(String args[]) {
+        double amountInAccount;
+        amountInAccount = 50.22;
+        amountInAccount = amountInAccount + 1000000.00;
+
+        System.out.print("На вашем счету сейчас ");
+        System.out.print(amountInAccount);
+        System.out.println(" долларов.");
+
+    }
+    
+ }
